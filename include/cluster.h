e1a9063db94969ba280c69ef1246ad098264fcf5
Projectius_student <sprojectiustech@gmail.com>
2024-11-29 03:30:47 +0300

    cluster control works

--- include/cluster.h
+++ include/cluster.h
@@ -2,14 +2,17 @@
 
 #include "smartQueue.h"
 #include <string>
+#include<iostream>
 
 using namespace std;
 
-struct Task
+class Task
 {
+public:
 	string id;
 	int ncores;
 	ull cycles;
+	Task(string id, int cores, ull cycles) :id(id), ncores(cores), cycles(cycles) {}
 };
 
 
@@ -21,23 +24,66 @@ class Cluster
 	int cores;
 	smart_queue<Task> q;
 	vector<Task> engaged;
-	int busy;
+	int busy = 0;
 
-	ull tick;
+	ull tick = 0;
 
 	//веса для распределения задач
-	int cyclesW = 2;
-	int coresW = 16;
+	int cyclesW = 1;
+	int coresW = 4;
 
 	int completed = 0;
 
 public:
 
-	Cluster(int cpu_count) :cores(cpu_count) {}
+	Cluster(int cpu_count) :cores(cpu_count) { //cout << "CORES " << cores << endl; 
+	}
 
-	void addTask(Task& t);
-	void process();
+	void process()
+	{
+		Task* candidate = 0;
+
+		candidate = &(q.top());
+		while (candidate && (candidate->ncores <= cores - busy))
+		{
+			engaged.push_back(*candidate);
+			busy += candidate->ncores;
+
+			//cout << tick<<" <<<<    engaged: " << candidate->id << " free "<< (cores - busy) << endl;
+
+			q.pop();
+			candidate = &(q.top());
+		}
+		
+
+		for (int i = 0;i < engaged.size();i++)
+		{
+			engaged[i].cycles -= 1;
+			//cout << tick << " <<<<    updated: " << engaged[i].id << " remaining " << engaged[i].cycles << endl;
+		}
+
+		for (int i = engaged.size()-1;i >=0;i--)
+		{
+			if (engaged[i].cycles <= 0)
+			{
+				busy -= engaged[i].ncores;
+				//cout << tick << " <<<<    ended: " << engaged[i].id << " free " << (cores - busy) << endl;
+				engaged.erase(engaged.begin() + i);
+				completed++;
+			}
+		}
+		tick += 1;
+	}
+
+	int getCompleted() { return completed; }
+
+	void addTask(Task& t)
+	{
+		q.insert(t, t.cycles * cyclesW + t.ncores * coresW + tick);
+	}
+	
 
 	vector<Task> getEngagedList() { return engaged; }
 
 };
+
--- test/test_cluster.cpp
+++ test/test_cluster.cpp
@@ -15,3 +15,74 @@ TEST(Cluster, can_add_tasks)
 	ASSERT_NO_THROW(c.addTask(t));
 }
 
+TEST(Cluster, task_in_progress)
+{
+	Cluster c(4);
+	Task t{ "T", 1, 10 };
+	c.addTask(t);
+	c.process();
+	ASSERT_EQ(c.getEngagedList()[0].id, t.id);
+}
+
+TEST(Cluster, can_complete_task)
+{
+	Cluster c(4);
+	Task t{ "T", 1, ull(1) };
+	c.addTask(t);
+	ASSERT_EQ(c.getCompleted(), 0);
+	for(int i=0;i<10;i++)
+		c.process();
+	ASSERT_EQ(c.getCompleted(), 1);
+}
+
+TEST(Cluster, doing_light_tasks_when_cant_heavy)
+{
+	Cluster c(8);
+	Task big{ "big", 6, 20 };
+	Task smol{ "smol", 2, 10 };
+	c.addTask(smol);
+	c.addTask(smol);
+	c.addTask(big);
+	c.addTask(smol);
+	c.addTask(smol);
+	for (int i = 0;i < 5;i++)
+		c.process();
+	ASSERT_EQ(c.getEngagedList().size(), 4);
+	for (int i = 0;i < 6;i++)
+		c.process();
+	ASSERT_EQ(c.getEngagedList().size(), 1);
+}
+
+TEST(Cluster, heavy_task_cant_be_delayed_forever) //лёгкие задачи добавляются бесконечно, но приоритетная система со временем перестанет добавлять их в начало очереди
+{
+	Cluster c(8);
+	Task big{ "big", 6, 10 };
+	Task smol{ "smol", 3, 3 };
+	c.addTask(smol);
+	c.addTask(smol);
+	c.addTask(big);
+
+	bool found = 0;
+	for (int i = 0;i < 500;i++)
+	{
+		c.addTask(smol);
+		c.process();
+		auto e = c.getEngagedList();
+		for (int j = 0;j < e.size();j++)
+		{
+			if (e[j].id == "big")
+			{
+				found = 1;
+				break;
+			}
+		}
+		if (found)
+			break;
+	}
+
+
+		ASSERT_TRUE(found);
+}
+
+
+
